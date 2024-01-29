9b2778eaa8a59d3d81e5da0d82c2f26540986603
Mozammel169649 <mozammel169649@gmail.com>
2024-01-30 01:16:51 +0600

    distruction data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useReducer } from "react";
+
 import { useDispatch, useSelector } from "react-redux";
+// import { decrement, increment, reset } from "./features/counter/counterSlice";
 import counterSlice from "./features/counter/counterSlice"
 
 function App() {
   const count = useSelector(state => state.counter);
+  const {increment,decrement,reset} = counterSlice.actions;
   const dispatch = useDispatch();
 
   console.log(count);
@@ -14,9 +16,9 @@ function App() {
         <center> 
         <br/>   
            <h2> Count : { count.value } </h2>  <br/>    
-           <button onClick={()=>dispatch(counterSlice.actions.increment())} style={{margin:"10px"}} >Increment</button>
-           <button onClick={()=>dispatch(counterSlice.actions.decrement())} style={{margin:"10px"}}>Decrement</button>
-           <button onClick={()=>dispatch(counterSlice.actions.reset())} style={{margin:"10px"}}>Reset</button>
+           <button onClick={()=>dispatch(increment())} style={{margin:"10px"}} >Increment</button>
+           <button onClick={()=>dispatch(decrement())} style={{margin:"10px"}}>Decrement</button>
+           <button onClick={()=>dispatch(reset())} style={{margin:"10px"}}>Reset</button>
         </center>
       </header>
 
